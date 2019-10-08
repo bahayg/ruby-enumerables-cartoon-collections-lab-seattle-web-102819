aee1421720245ce0b9c8f5cf941346070f832026
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-08 17:32:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,9 +2,9 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
-
-
-
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect {|calls| 
+end
 
 
 
