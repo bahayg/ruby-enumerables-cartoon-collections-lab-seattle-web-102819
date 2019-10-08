1256aaaa85e05f8a708cb893279bd0bde0e69fe6
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-08 17:24:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|name, index|}
-      puts "#{index + 1}. #{name}"
+  dwarves.each_with_index {|name, index|
+      puts "#{index + 1}. #{name}"}
 end
 
 
