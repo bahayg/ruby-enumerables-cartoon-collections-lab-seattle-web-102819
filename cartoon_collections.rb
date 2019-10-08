e25c2608800b0eac95913c7c2cf953f73433c7a1
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-08 17:51:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,9 +14,3 @@ def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
   strings.find {|cheese| cheese_types.include?(cheese)}
 end
-
-
-# def find_the_cheese(array)
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-#   array.find {|cheese| cheese_types.include?(cheese)} 
-# end
